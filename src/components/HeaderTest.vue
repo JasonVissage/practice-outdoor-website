b3f17121b364842b135ec3911e46e3c449653c24
Jason Vissage <jasonvissage@yahoo.com>
2022-05-23 15:52:51 -0500

    add tailwindcss and headertest component

---
+++ src/components/HeaderTest.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-blue-400">
+    <div class="text-center text-5xl hover:bg-blue-600">This is a header</div>
+  </div>
+</template>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex">
+      <img class="" alt="Mountains reflecting off lake" src="../assets/home_photo.jpg" />
+  </div>
+  <div>
+    <h1 class="text-xl">Hello Vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
